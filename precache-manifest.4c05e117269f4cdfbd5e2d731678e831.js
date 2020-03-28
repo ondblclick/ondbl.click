6da4f309d029591df8409e1eacd43131143bd5ad
Bohdan Hontarenko <tuoefil@gmail.com>
2020-03-28 23:29:47 +0200

    Updates

---
+++ precache-manifest.4c05e117269f4cdfbd5e2d731678e831.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53c5b9ddd3b5a4897ea34187c31cbd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901594b1de8c1b3c4424",
+    "url": "/static/css/main.aa48c1b7.chunk.css"
+  },
+  {
+    "revision": "ecc5f3d1ad52624b7890",
+    "url": "/static/js/2.ac737194.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ac737194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "901594b1de8c1b3c4424",
+    "url": "/static/js/main.892a2466.chunk.js"
+  },
+  {
+    "revision": "cd9dac055e9b6fbc717e",
+    "url": "/static/js/runtime-main.1bbbc98a.js"
+  },
+  {
+    "revision": "aba7d1340ed427ad924d6631d5cb600f",
+    "url": "/static/media/dear-esther-1.aba7d134.svg"
+  },
+  {
+    "revision": "68f6dde3a776705a72c1cf98ab292c61",
+    "url": "/static/media/dear-esther-2.68f6dde3.svg"
+  },
+  {
+    "revision": "f5785800995800f7b17f78e0adf8f84b",
+    "url": "/static/media/dear-esther-3.f5785800.svg"
+  },
+  {
+    "revision": "9176b57865889e9aab559344bb297b8f",
+    "url": "/static/media/fullscreen-24px.9176b578.svg"
+  },
+  {
+    "revision": "578b7cb1b4f0f4a1c2fa0f02b30524f1",
+    "url": "/static/media/fullscreen_exit-24px.578b7cb1.svg"
+  },
+  {
+    "revision": "e24c8e23bb08cf085d8fd8271002bddd",
+    "url": "/static/media/home-24px.e24c8e23.svg"
+  },
+  {
+    "revision": "a476ae58b34bb285005fdce21f110674",
+    "url": "/static/media/settings-24px.a476ae58.svg"
+  },
+  {
+    "revision": "43a3aab23e3037f8c278c7641595a613",
+    "url": "/static/media/volume_off-24px.43a3aab2.svg"
+  },
+  {
+    "revision": "4944b4894f7cb072b77c284f1407ab28",
+    "url": "/static/media/volume_up-24px.4944b489.svg"
+  }
+]);
